b87931bc0faae63f6c0a8c6ba3c850ea322cfae2
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-08-07 17:19:02 +0600

    add task-1

--- task-1.js
+++ task-1.js
@@ -1,1 +1,6 @@
-console.log('lets go shehab')
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+let newArr = [];
+for(let i = colors.length-1; i>=0; i--){
+    newArr.push(colors[i]);
+}
+console.log(newArr);
